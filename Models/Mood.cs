0f69cc952355876dc3b2ee9443a249685e8e717e
ST10320176 <st10320176@myemeris.edu.za>
2026-06-19 12:11:18 +0200

    Added Mood model with Name, Emoji, Color, Description, and Songs list

---
+++ Models/Mood.cs
@@ -0,0 +1,11 @@
+﻿namespace MoodPlay.Models
+{
+    public class Mood
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Emoji {  get; set; }  = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public string Description {  get; set; } = string.Empty;
+        public List<Song> Songs { get; set; } = new List<Song>();
+    }
+}
