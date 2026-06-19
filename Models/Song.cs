0bb4660b1b4189b737265e4553f03ca9654c9786
ST10320176 <st10320176@myemeris.edu.za>
2026-06-19 11:51:01 +0200

    Added Song model with Title, Artist, and YouTubeUrl properties

---
+++ Models/Song.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodPlay.Models
+{
+    public class Song
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Artist { get; set; } = string.Empty;
+        public string YouTubeUrl { get; set; } = string.Empty;
+
+    }
+}
